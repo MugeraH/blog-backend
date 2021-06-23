e1a6c8d969a2420a22992f95044e109de8b97826
MugeraH <bwosihughes24@gmail.com>
2021-06-23 20:44:31 +0300

    add blog model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+class Blog(models.Model):
+    LIFE = 'life'
+    BUSINESS = 'business'
+    TRAVEL = 'travel'
+    RANDOM_THOUGHTS = 'random_thoughts'
+    CARRER = 'carrer'
+    ENTERTAINMENT = 'entertainment'
+    
+    
+    CHOICES_CATEGORY = {
+        (LIFE,'Life'),
+        (BUSINESS,'Business'),
+        (RANDOM_THOUGHTS,'Random_thoughts'),
+        (TRAVEL,'Travel'),
+        (CARRER,'Carrer'),
+        (ENTERTAINMENT,'Entertainment')
+    }   
+    
+    title = models.CharField(max_length=100)
+    category = models.CharField(max_length=25 ,choices=CHOICES_CATEGORY, default=LIFE)
+    description = models.TextField(null=True)
+    image = CloudinaryField('image',null=True)
+    author = models.ForeignKey(User,related_name='leads',on_delete=models.CASCADE)
+    blog = models.TextField(null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    
